1455fc809d2c354671a4d2bc019e1dea0f1b8976
nghia4mt <minhnghia0399@gmail.com>
2024-01-19 18:01:23 +0700

    fix: remove backup hyprland & waybar

--- script/backup-dotfiles.sh
+++ script/backup-dotfiles.sh
@@ -9,17 +9,11 @@ $SYNC ~/.config/nvim/ $DOTFILES/nvim/
 #Backup kitty
 $SYNC ~/.config/kitty/ $DOTFILES/kitty/
 
-#Backup hyprland
-$SYNC ~/.config/hypr/ $DOTFILES/hypr/
-
 #Backup fcitx5
 $SYNC ~/.config/fcitx5/ $DOTFILES/fcitx5/
 
 #Backup rofi
 $SYNC ~/.config/rofi/ $DOTFILES/rofi/
-
-#Backup waybar
-$SYNC ~/.config/waybar/ $DOTFILES/waybar/
 
 #Backup starship
 cp -r ~/.config/starship.toml $DOTFILES
