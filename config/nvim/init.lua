1e28535eeebd2f20fc2b57b7a24ed47359af692d
nghia4mt <minhnghia0399@gmail.com>
2024-02-01 16:54:43 +0700

    fea:neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,9 @@
 require("config.lazy")
-local opt = vim.opt
-opt.number = true             -- Show line number
-opt.clipboard = "unnamedplus" -- Sync clipboard
-opt.undofile = true           -- Enable undofile
-opt.undolevels = 10000        -- Set level undofile
---opt.undodir = os.getenv("HOME") .. "/.cache/nvim" -- Set location cache undo
-opt.background = "dark"       -- or "light" for light mode
+require("plugins.coc")
 
---netrw
-vim.api.nvim_set_keymap('n', '<leader>b', ':Lex<CR>', { noremap = true, silent = true })
-vim.cmd('let g:netrw_winsize=25')
-
---Code_runner
-vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+vim.opt.number = true --show line number
+vim.opt.clipboard = "unnamedplus" --sync clipboard
+vim.opt.undofile = true --enable undo file
+vim.opt.undolevels = 1000
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim" -- Set location cache undo
+vim.opt.background = "dark"
