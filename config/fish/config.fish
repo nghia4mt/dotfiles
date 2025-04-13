cfa252156c8d1197542bcdb20771d41fb5c0246f
nghia4mt <minhnghia0399@gmail.com>
2025-04-13 09:51:36 +0700

    del:i3wm

--- config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-zoxide init fish | source
-source ~/.config/fish/alias.fish
-export PATH="$PATH:/opt/nvim-linux64/bin"
